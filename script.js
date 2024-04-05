aa0f90fa5d67ea18db3f26bc36da1bd3329e7715
kvgvmi <oucheikhimane7@gmail.com>
2024-04-05 03:30:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -54,4 +54,37 @@ canvas.addEventListener("mouseup", function () {
       this.spriteHeight = 327; // three lines
     }
 
-    
+    //to move the player towards the mouse
+  update() {
+    const dx = this.x - mouse.x; // dx = distance from x
+    const dy = this.y - mouse.y; // dy = distance from y
+    let theta = Math.atan2(dy, dx);
+    this.angle = theta;
+
+
+    //if current mouse x position is not equal to current player's position 
+    if (mouse.x != this.x) { 
+      this.x -= dx / 20;
+    }
+    if (mouse.y != this.y) {//madernach else if hena 7it bghinahum bjoj ikhedmo de9a we7da
+      this.y -= dy / 20;// "20" to add animation or else the player will move fast to the mouse position 
+    }
+
+
+    //------------------------------??-------------------------------
+    if (gameFrame % 5 == 0) {
+      this.frame++;
+      if (this.frame >= 12) this.frame = 0;
+      if (this.frame == 3 || this.frame == 7 || this.frame == 11) {
+        this.frameX = 0;
+      } else {
+        this.frameX++;
+      }
+      if (this.frame < 3) this.frameY = 0;
+      else if (this.frame < 7) this.frameY = 1;
+      else if (this.frame < 11) this.frameY = 2;
+      else this.frameY = 0;
+    }
+  }
+
+  
