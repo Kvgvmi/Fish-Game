d1b0febd383e37561210c68574ee02cac6edd0ba
kvgvmi <oucheikhimane7@gmail.com>
2024-04-05 03:26:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ ctx.font = "40px Georgia";
 let gameSpeed = 1;
 let gameOver = false;
 
+// Mouse Interactivity
+let canvasPosition = canvas.getBoundingClientRect();
+
+//used to track the mouse position and whether it's clicked or not
+const mouse = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  click: false,
+};
+
+//when the mouse button is pressed down on the canvas. It updates the mouse object with the current mouse position relative to the canvas (taking into account the canvas position on the page).
+canvas.addEventListener("mousedown", function (event) {
+  mouse.click = true;
+  mouse.x = event.x - canvasPosition.left;//to get the position relative to the canvas
+  mouse.y = event.y - canvasPosition.top;
+});
+
